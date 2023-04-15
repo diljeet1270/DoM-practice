90c0374a5dea223179cbef2993be526d6626f51a
Deljeet Kumar <uic.21mca2073@gmail.com>
2023-04-15 18:47:33 +0530

    DoM practice

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 let students = [
-    {name: 'Rahul Chandel', Maths: 50, computer: 79},
-    {name: 'Rohini Manhas', Maths: 40, computer: 69},
-    {name: 'Ankush Katoch', Maths: 50, computer: 79},
-    {name: 'Kuldeep singh', Maths: 50, computer: 79},
-    {name: 'Akash Baloria', Maths: 50, computer: 79},
-    {name: 'Shivani Chandel', Maths: 50, computer: 79},
+    {name: 'Rahul Chandel', Maths: 82, computer: 88},
+    {name: 'Rohini Manhas', Maths: 44, computer: 55},
+    {name: 'Ankush Katoch', Maths: 91, computer: 73},
+    {name: 'Kuldeep singh', Maths: 60, computer: 89},
+    {name: 'Akash Baloria', Maths: 58, computer: 60},
+    {name: 'Shivani Chandel', Maths: 42, computer: 47},
     {name: 'Shivali Kotwal', Maths: 50, computer: 79},
     {name: 'Vikas', Maths: 50, computer: 79},
     ]
+    let highlightMarks = false;
     function showData(){
         let arr = students.map(function(ele){
             let str = '<tr>';
                 str+= '<td class=\'td\'>' + ele.name + '</td>';
-                str+= '<td class=\'td\'>' + ele.Maths + '</td>';
-                str+= '<td class=\'td\'>' + ele.computer + '</td>';
+                str+= '<td class=\''+getClassMarks(ele.Maths)+'\'>' + ele.Maths + '</td>';
+                str+= '<td class=\''+getClassMarks(ele.computer)+'\'>' + ele.computer + '</td>';
                 str+= '</tr>'
                 return str;
         });
@@ -25,4 +26,16 @@ let students = [
             let html = '<table class=\'table\'>'+header + arr.join('')+'</table>';
             let element = document.getElementById('data');
             element.innerHTML = html;
+    }
+    function getClassMarks(marks){
+        if(marks>=75 && highlightMarks)
+            return 'td score70'
+        else if(marks<=50 && highlightMarks)
+            return 'td score50'
+        else
+            return 'td'
+    }
+    function highMarks(){
+        highlightMarks = true;
+        showData();
     }
